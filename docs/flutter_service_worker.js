f6ad7cb0f29017a5d54fce36d6230e180fa12fe6
@imagistlondon <imagistlondon@users.noreply.github.com>
2020-11-08 21:45:46 +0000

    docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -66,8 +66,8 @@ const RESOURCES = {
 "apple-touch-icon.png": "ae03aa0f1a3dba39c37ffd7ae02863bc",
 "font-untitled-sans-web-light.woff2": "ab8dc13c2dd328a8498a7e573269da16",
 "version.json": "fb800d37cb51ce25f91a8114a375508b",
-"index.html": "56d901e16e21022bc9198e4231941dcd",
-"/": "56d901e16e21022bc9198e4231941dcd",
+"index.html": "f8a3fd7894141c82916d8dfd24ff1d7b",
+"/": "f8a3fd7894141c82916d8dfd24ff1d7b",
 "font-saol-text-light.woff": "af367118659c3dcdb6e18409b688d97a"
 };
 
