6e66283928e5101eb335bdcf57421dccbbeba180
@imagistlondon <imagistlondon@users.noreply.github.com>
2020-12-22 13:03:39 +0000

    docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -66,11 +66,11 @@ const RESOURCES = {
 "assets/AssetManifest.json": "34e9b5b9385a2ded0da14067d2549ed8",
 "assets/FontManifest.json": "a506737225df98745abf78d11ebbf28a",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"index.html": "fa56d5e85e2a457a1cdc49f74187a96a",
-"/": "fa56d5e85e2a457a1cdc49f74187a96a",
+"index.html": "e5baf26830fc8e4db22f19279329d11f",
+"/": "e5baf26830fc8e4db22f19279329d11f",
 "font-untitled-sans-web-light.woff2": "ab8dc13c2dd328a8498a7e573269da16",
 "version.json": "fb800d37cb51ce25f91a8114a375508b",
-"main.dart.js": "cfa5a06d30474f9978bd21959abb06ad",
+"main.dart.js": "c9a7a311f3cdc11dcc91f58a70f34b44",
 "font-saol-text-light.eot": "cf44e4b6266324651556fb56186e6400"
 };
 
