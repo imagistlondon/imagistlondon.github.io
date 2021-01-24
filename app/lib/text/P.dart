d5b97e9a5ce978a6e3ab4ef2a20a0df46c543583
chrisdchristo <chrisdchristo>
2021-01-24 22:32:36 +0100

    Make P markdown powered

--- app/lib/text/P.dart
+++ app/lib/text/P.dart
@@ -1,6 +1,8 @@
 import 'package:app/config/Break.dart';
 import 'package:app/config/Design.dart';
+import 'package:app/util/URL.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 // <p>
 class P extends StatelessWidget {
@@ -10,6 +12,7 @@ class P extends StatelessWidget {
       this.textAlign = TextAlign.left,
       this.style,
       this.selectable = false,
+      this.markdown = false,
       this.opacity})
       : super(key: key);
 
@@ -17,6 +20,7 @@ class P extends StatelessWidget {
   final TextAlign textAlign;
   final TextStyle style;
   final bool selectable;
+  final bool markdown;
   final double opacity;
 
   @override
@@ -30,8 +34,26 @@ class P extends StatelessWidget {
     // opacity
     if (opacity != null) s = s.copyWith(color: s.color.withOpacity(opacity));
 
-    return selectable
-        ? SelectableText(text, textAlign: textAlign, style: s)
-        : Text(text, textAlign: textAlign, style: s);
+    return markdown
+        ? MarkdownBody(
+            styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context))
+                .copyWith(
+                    h1: Design.H1(context),
+                    h2: Design.H2(context),
+                    h3: Design.H2(context),
+                    h4: Design.H2(context),
+                    h5: Design.H2(context),
+                    h6: Design.H2(context),
+                    p: Design.P(context)),
+            data: text,
+            // how to open links
+            onTapLink: (text, href, title) {
+              URL.open(href);
+            },
+            // selectable text
+            selectable: true)
+        : selectable
+            ? SelectableText(text, textAlign: textAlign, style: s)
+            : Text(text, textAlign: textAlign, style: s);
   }
 }
