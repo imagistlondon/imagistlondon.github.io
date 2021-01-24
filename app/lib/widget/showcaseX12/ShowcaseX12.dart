e87bf9c517c7dfeb3780b72cdcb13addcbc02675
chrisdchristo <chrisdchristo>
2021-01-24 16:53:33 +0100

    Showcasex12 HeaderBulletLinksX1

--- app/lib/widget/showcaseX12/ShowcaseX12.dart
+++ app/lib/widget/showcaseX12/ShowcaseX12.dart
@@ -1,9 +1,11 @@
 import 'package:app/Index.dart';
+import 'package:app/config/Break.dart';
 import 'package:app/config/Content.dart';
 import 'package:app/config/Design.dart';
 import 'package:app/util/IndexNotifier.dart';
 import 'package:app/util/L1.dart';
 import 'package:app/util/StudyEnabledNotifier.dart';
+import 'package:app/widget/header/HeaderBulletLinksX1.dart';
 import 'package:app/widget/showcaseX12/ShowcaseImageX12.dart';
 import 'package:app/widget/showcaseX12/ShowcaseLinkX12.dart';
 import 'package:flutter/material.dart';
@@ -13,11 +15,13 @@ class ShowcaseX12 extends StatelessWidget {
       {Key key,
       @required this.contentVN,
       @required this.indexVN,
+      @required this.bulletsEnabledVN,
       @required this.studyEnabledVN})
       : super(key: key);
 
   final ValueNotifier<Content> contentVN;
   final IndexNotifier indexVN;
+  final ValueNotifier<bool> bulletsEnabledVN;
   final StudyEnabledNotifier studyEnabledVN;
 
   @override
@@ -36,6 +40,11 @@ class ShowcaseX12 extends StatelessWidget {
                     child: Column(children: <Widget>[
               // HEADER CLEARANCE
               clearance,
+              // HEADER BULLETS X1
+              Break.x1(context)
+                  ? HeaderBulletLinksX1(
+                      indexVN: indexVN, bulletsEnabledVN: bulletsEnabledVN)
+                  : SizedBox.shrink(),
               // PROJECTS
               for (final Project project in contentVN.value.SHOWCASE_PROJECTS)
                 Column(
