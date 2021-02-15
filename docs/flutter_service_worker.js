ca8f741616c0bff4e6abc503777a95239e05dd63
@imagistlondon <imagistlondon@users.noreply.github.com>
2021-02-15 11:32:50 +0000

    docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "8b600d41b98db9e13fef155c4a53c465",
+  "main.dart.js": "9796ef4b983c4052abc1284ad3f8e803",
 "favicon.ico": "19a0349ef88d75cb2624b98a4e03e2fa",
 "apple-touch-icon.png": "ae03aa0f1a3dba39c37ffd7ae02863bc",
 "favicon-32x32.png": "8b9d8a3841cfb6b20fb023430c3117dd",
@@ -158,8 +158,8 @@ const RESOURCES = {
 "version.json": "fb800d37cb51ce25f91a8114a375508b",
 "manifest.json": "1063745475257257f2aa77a8c929cd93",
 "404.html": "f4e3edb53349945387e617abbd0fd231",
-"index.html": "85b362efb5cad504799221048cf1fdab",
-"/": "85b362efb5cad504799221048cf1fdab",
+"index.html": "bbacc4d1fffc19153120e92202d6bcb5",
+"/": "bbacc4d1fffc19153120e92202d6bcb5",
 "android-chrome-512x512.png": "78b6fbf11ebd07343a7b1584195bbee7",
 "android-chrome-192x192.png": "b86e1bedfeba2b7f0ee344c2c4007125"
 };
