65c137aa49b65e1a7248677aa49b09185b357310
chrisdchristo <chrisdchristo>
2021-02-21 20:21:00 +0100

    chris/remove-markdown

--- app/lib/text/P.dart
+++ app/lib/text/P.dart
@@ -2,7 +2,7 @@ import 'package:app/config/Break.dart';
 import 'package:app/config/Design.dart';
 import 'package:app/util/URL.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+// import 'package:flutter_markdown/flutter_markdown.dart';
 
 // <p>
 class P extends StatelessWidget {
@@ -34,26 +34,27 @@ class P extends StatelessWidget {
     // opacity
     if (opacity != null) s = s.copyWith(color: s.color.withOpacity(opacity));
 
-    return markdown
-        ? MarkdownBody(
-            styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context))
-                .copyWith(
-                    h1: Design.H1(context),
-                    h2: Design.H2(context),
-                    h3: Design.H2(context),
-                    h4: Design.H2(context),
-                    h5: Design.H2(context),
-                    h6: Design.H2(context),
-                    p: Design.P(context)),
-            data: text,
-            // how to open links
-            onTapLink: (text, href, title) {
-              URL.open(href);
-            },
-            // selectable text
-            selectable: true)
-        : selectable
-            ? SelectableText(text, textAlign: textAlign, style: s)
-            : Text(text, textAlign: textAlign, style: s);
+    // return markdown
+    //     ? MarkdownBody(
+    //         styleSheet: MarkdownStyleSheet.fromTheme(Theme.of(context))
+    //             .copyWith(
+    //                 h1: Design.H1(context),
+    //                 h2: Design.H2(context),
+    //                 h3: Design.H2(context),
+    //                 h4: Design.H2(context),
+    //                 h5: Design.H2(context),
+    //                 h6: Design.H2(context),
+    //                 p: Design.P(context)),
+    //         data: text,
+    //         // how to open links
+    //         onTapLink: (text, href, title) {
+    //           URL.open(href);
+    //         },
+    //         // selectable text
+    //         selectable: true)
+    //     : selectable
+    return selectable
+        ? SelectableText(text, textAlign: textAlign, style: s)
+        : Text(text, textAlign: textAlign, style: s);
   }
 }
