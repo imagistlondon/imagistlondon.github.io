6159088cdb6dab0e9aa6e968e2b690d5c284d8a3
chrisdchristo <chrisdchristo>
2021-04-07 00:42:42 +0200

    v2.0.4

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:app/App.dart';
 
 void main() {
-  print('v2.0.3');
+  print('v2.0.4');
 
   // set no # (hashbang) for web
   setUrlStrategy(PathUrlStrategy());
